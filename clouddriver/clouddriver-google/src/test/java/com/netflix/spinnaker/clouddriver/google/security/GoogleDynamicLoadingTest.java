f723d94d0dfab446f84e0b0a013da1d5b877c006
shlomodaari <shlomo.daari@harness.io>
2025-09-29 15:15:01 -0700

    feat(google): Implement Google Dynamic Credential Loading
    This commit:
    1. Creates GoogleCredentialsParser for dynamic credential loading
    2. Implements GoogleCredentialsInitializer with CredentialsInitializerSynchronizable support
    3. Adds unit tests for credential loading functionality
    
    These changes enable dynamic credential loading for Google Cloud provider.

---
+++ clouddriver/clouddriver-google/src/test/java/com/netflix/spinnaker/clouddriver/google/security/GoogleDynamicLoadingTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 Harness, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.clouddriver.google.security;
+
+import static org.mockito.Mockito.*;
+
+import com.netflix.spinnaker.clouddriver.security.CredentialsInitializerSynchronizable;
+import com.netflix.spinnaker.credentials.definition.AbstractCredentialsLoader;
+import org.junit.jupiter.api.Test;
+
+public class GoogleDynamicLoadingTest {
+
+  @Test
+  public void testCredentialsInitializerSynchronizableCallsLoader() {
+    // Arrange
+    AbstractCredentialsLoader mockLoader = mock(AbstractCredentialsLoader.class);
+    GoogleCredentialsInitializer initializer = new GoogleCredentialsInitializer();
+
+    CredentialsInitializerSynchronizable synchronizable =
+        initializer.googleCredentialsInitializerSynchronizable(mockLoader);
+
+    // Act
+    synchronizable.synchronize();
+
+    // Assert
+    verify(mockLoader, times(1)).load();
+    System.out.println("TEST PASSED: Google synchronize method correctly calls loader.load()");
+  }
+}
